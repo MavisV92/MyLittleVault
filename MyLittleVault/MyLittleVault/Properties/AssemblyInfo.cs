9c93900290319eebf5a0185d7ac3caae8389d9cd
MavisV92 <73093294+MavisV92@users.noreply.github.com>
2022-06-18 13:40:04 +0300

    Start

--- MyLittleVault/MyLittleVault/Properties/AssemblyInfo.cs
+++ MyLittleVault/MyLittleVault/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("WindowsFormsApp1")]
+[assembly: AssemblyTitle("MyLittleVault")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyProduct("MyLittleVault")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("dce12db4-53bf-482e-bd23-8d4e5efc30d1")]
+[assembly: Guid("5aad2b41-45eb-427f-b5fc-3308cc3706db")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
